11dcfbe016328e705b4ccb30cf59c81be68aa97f
gabrielledelforge <gabrielle.delforge@berkeley.edu>
2019-10-25 16:25:01 -0700

    Work on pure frontend for create event page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import EventCreator from "./EventCreator";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
-export default function App() {    
-  
+
+export default function App() {
   return (
     <div className="App">
-      <div className="navbar">Navbar</div>
+      <AppBar position="static" className="navbar">
+        <Typography variant="h6" className="navbar-text">
+          what2eat
+        </Typography>
+      </AppBar>
       <EventCreator />
     </div>
   );
-
 }
-
-
-
