cbbcca56105dca5443361638bebef7a30d821a39
Ian Galbraith <igalbraith27@gmail.com>
2019-10-26 14:08:49 -0700

    Added starter code for API stuff

--- src/EventCreator.js
+++ src/EventCreator.js
@@ -9,27 +9,55 @@ import {
 } from "@material-ui/pickers";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-
+import * as Api from "./api/Api"
 export default class EventCreator extends Component {
   constructor(props) {
     super(props);
-    this.state = { selectedDate: new Date() };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      creatorName : "",
+      selectedDate: new Date(),
+      eventName: "",
+      friends: "",
+     };
   }
 
   handleDateChange = date => {
     this.setState({ selectedDate: date });
   };
+  handleNameChange = evt => {
+    this.setState({ creatorName: evt.target.value})
+  };
+  handleFriendChange = evt => {
+    this.setState({ friends: evt.target.value})
+  };
+  handleEventChange = evt => {
+    this.setState({ eventName: evt.target.value})
+  };
+  handleSubmit(evt) {
+    evt.preventDefault();
+    console.log("Creator Name: ", this.state.creatorName, "Event Title: ", this.state.eventName, "DATE: ", this.state.selectedDate)
+    let apiResp = Api.create_event(this.state.eventName, this.state.selectedDate, this.state.creatorName, "Berkeley")
+    console.log(apiResp);
+  };
 
   render() {
     return (
       <Paper className="create-event">
-        <form className="create-event-field" noValidate autoComplete="off">
+        <form className="create-event-field" noValidate autoComplete="off" onSubmit = {this.handleSubmit}>
           <TextField
             fullWidth
             id="standard-uncontrolled"
-            label="Event Title"
-            defaultValue="Make a cool event title"
+            label="What's Your Name?"
             margin="normal"
+            onChange = {this.handleNameChange}
+          />
+          <TextField
+            fullWidth
+            id="standard-uncontrolled"
+            label="Make A Cool Event Title!"
+            margin="normal"
+            onChange = {this.handleEventChange}
           />
 
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -63,15 +91,10 @@ export default class EventCreator extends Component {
             />
           </MuiPickersUtilsProvider>
 
-          <TextField
-            fullWidth
-            id="standard-uncontrolled"
-            label="Invite Friends"
-            defaultValue="bob@example.com"
-            margin="normal"
-          />
+
 
           <Button
+            type = "submit"
             variant="contained"
             color="primary"
             className="create-event-submit"
