22138a5f9fd54a0efc7c473c83a3d9c9a283b148
Ian Galbraith <igalbraith27@gmail.com>
2019-10-25 17:11:08 -0700

    Initial Restaurant Poll commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import EventCreator from "./EventCreator";
+import RestaurantPoll from "./RestaurantPoll";
 
-export default function App() {    
-  
+export default function App() {
+
   return (
     <div className="App">
       <div className="navbar">Navbar</div>
-      <EventCreator />
+      <RestaurantPoll />
     </div>
   );
 
 }
-
-
-
